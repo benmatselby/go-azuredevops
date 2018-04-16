07db32bb509c6c8967cebb3aa816ac9cf3eb72df
Ben Selby <ben.selby@emisgroupplc.com>
2018-04-16 08:08:41 +0100

    Update the formatting for GoDoc

--- vsts/doc.go
+++ vsts/doc.go
@@ -1,13 +1,13 @@
 /*
 go-vsts is a Go client library for accessing the Visual Studio Team Services API.
 Installation
-    $ go get github.com/benmatselby/go-vsts/vsts
+	$ go get github.com/benmatselby/go-vsts/vsts
 	or
 	$ dep ensure -add github.com/benmatselby/go-vsts/vsts
 Usage
 Interaction with the VSTS API is done through a Client instance.
-    import "github.com/benmatselby/go-vsts/vsts
-    v := vsts.NewClient(account, project, token)
+	import "github.com/benmatselby/go-vsts/vsts
+	v := vsts.NewClient(account, project, token)
 Services
 The client has services that you can use to access resources from the API:
 	iterations, error := v.Iterations.List(team)
