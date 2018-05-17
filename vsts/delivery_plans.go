159cf3712229d64621330ca413a486b77d115205
Ben Selby <benmatselby@gmail.com>
2018-05-17 22:31:33 +0100

    Provide a GetTimeLine call for the delivery plans

--- vsts/delivery_plans.go
+++ vsts/delivery_plans.go
@@ -14,6 +14,15 @@ type DeliveryPlansListResponse struct {
 	Count         int            `json:"count"`
 }
 
+// DeliveryPlanTimeLine describes the delivery plan get response
+type DeliveryPlanTimeLine struct {
+	StartDate string         `json:"startDate"`
+	EndDate   string         `json:"endDate"`
+	ID        string         `json:"id"`
+	Revision  int            `json:"revision"`
+	Teams     []DeliveryTeam `json:"teams"`
+}
+
 // DeliveryPlan describes an plan
 type DeliveryPlan struct {
 	ID      string `json:"id"`
@@ -22,6 +31,28 @@ type DeliveryPlan struct {
 	Created string `json:"createdDate"`
 	URL     string `json:"url"`
 }
+
+// DeliveryTeam describes the teams in a specific plan
+type DeliveryTeam struct {
+	ID         string      `json:"id"`
+	Name       string      `json:"name"`
+	Iterations []Iteration `json:"iterations"`
+}
+
+const (
+	// DeliveryPlanWorkItemIDKey is the key for which part of the workItems[] slice has the ID
+	DeliveryPlanWorkItemIDKey = 0
+	// DeliveryPlanWorkItemIterationKey is the key for which part of the workItems[] slice has the Iteration
+	DeliveryPlanWorkItemIterationKey = 1
+	// DeliveryPlanWorkItemTypeKey is the key for which part of the workItems[] slice has the Type
+	DeliveryPlanWorkItemTypeKey = 2
+	// DeliveryPlanWorkItemNameKey is the key for which part of the workItems[] slice has the Name
+	DeliveryPlanWorkItemNameKey = 4
+	// DeliveryPlanWorkItemStatusKey is the key for which part of the workItems[] slice has the Status
+	DeliveryPlanWorkItemStatusKey = 5
+	// DeliveryPlanWorkItemTagKey is the key for which part of the workItems[] slice has the Tag
+	DeliveryPlanWorkItemTagKey = 6
+)
 
 // DeliveryPlansListOptions describes what the request to the API should look like
 type DeliveryPlansListOptions struct {
@@ -40,4 +71,21 @@ func (s *DeliveryPlansService) List(opts *DeliveryPlansListOptions) ([]DeliveryP
 	_, err = s.client.Execute(request, &response)
 
 	return response.DeliveryPlans, response.Count, err
+}
+
+// GetTimeLine will fetch the details about a specific delivery plan
+func (s *DeliveryPlansService) GetTimeLine(ID string) (*DeliveryPlanTimeLine, error) {
+	URL := fmt.Sprintf(
+		"_apis/work/plans/%s/deliverytimeline?api-version=5.0-preview.1",
+		ID,
+	)
+
+	request, err := s.client.NewRequest("GET", URL)
+	if err != nil {
+		return nil, err
+	}
+	var response DeliveryPlanTimeLine
+	_, err = s.client.Execute(request, &response)
+
+	return &response, err
 }
--- vsts/delivery_plans_test.go
+++ vsts/delivery_plans_test.go
@@ -29,6 +29,43 @@ const (
 		],
 		"count": 2
 	}`
+	deliveryPlanGetURL              = "/VSTS_Project/_apis/work/plans/7154147c-43ca-44a9-9df0-2fa0a7f9d6b2/deliverytimeline"
+	deliveryPlanGetTimeLineResponse = `
+	{
+		"id": "7154147c-43ca-44a9-9df0-2fa0a7f9d6b2",
+		"startDate": "2018-05-04T00:00:00+00:00",
+		"endDate": "2018-07-06T00:00:00+00:00",
+		"teams": [
+			{
+				"id": "c7d2dc3a-2d44-45e1-b1f1-ca2454ed368a",
+				"name": "Team One",
+				"iterations": [
+					{
+						"name": "Iteration One",
+						"path": "Project\\Team\\1",
+						"startDate": "2018-04-30T00:00:00Z",
+						"finishDate": "2018-05-11T00:00:00Z",
+						"workItems": [
+							[
+								1097,
+								"Project\\Team\\1",
+								"Feature",
+								null,
+								"Feaeture One",
+								"New",
+								"This is a tag",
+								"Project",
+								5,
+								"Project\\Team",
+								null
+							]
+						]
+					}
+				]
+			}
+		]
+	}
+	`
 )
 
 func TestDeliveryPlansService_List(t *testing.T) {
@@ -80,5 +117,47 @@ func TestDeliveryPlansService_List(t *testing.T) {
 				t.Fatalf("expected delivery plan count to be %d; got %d", tc.count, count)
 			}
 		})
+	}
+}
+
+func TestDeliveryPlansService_GetTimeLine(t *testing.T) {
+	c, mux, _, teardown := setup()
+	defer teardown()
+
+	planID := "7154147c-43ca-44a9-9df0-2fa0a7f9d6b2"
+
+	mux.HandleFunc(deliveryPlanGetURL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		json := deliveryPlanGetTimeLineResponse
+		fmt.Fprint(w, json)
+	})
+
+	timeline, err := c.DeliveryPlans.GetTimeLine(planID)
+	if err != nil {
+		t.Fatalf("returned error: %v", err)
+	}
+
+	if timeline.ID != "7154147c-43ca-44a9-9df0-2fa0a7f9d6b2" {
+		t.Fatalf("expected delivery plan id %s, got %s", planID, timeline.ID)
+	}
+
+	if timeline.Teams[0].Name != "Team One" {
+		t.Fatalf("expected delivery plan to have team[0].Name of %s, got %s", "Team One", timeline.Teams[0].Name)
+	}
+
+	if timeline.Teams[0].Iterations[0].Name != "Iteration One" {
+		t.Fatalf(
+			"expected delivery plan to have team[0].Iterations[0].Name of %s, got %s",
+			"Iteration One",
+			timeline.Teams[0].Name,
+		)
+	}
+
+	if timeline.Teams[0].Iterations[0].WorkItems[0][vsts.DeliveryPlanWorkItemIDKey].(float64) != 1097 {
+		t.Fatalf(
+			"expected delivery plan to have teams[0].Iterations[0].WorkItems[0][vsts.DeliveryPlanWorkItemIDKey] of %v, got %v",
+			1097,
+			timeline.Teams[0].Iterations[0].WorkItems[0][vsts.DeliveryPlanWorkItemIDKey].(float64),
+		)
 	}
 }
