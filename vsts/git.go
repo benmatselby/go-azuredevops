20c3a28d701160225f80081d6f2261dbb81f260a
Ben Selby <benmatselby@gmail.com>
2018-06-07 20:36:26 +0100

    Add the start of a Git API

---
+++ vsts/git.go
@@ -0,0 +1,70 @@
+package vsts
+
+import (
+	"fmt"
+	"time"
+)
+
+// GitService handles communication with the git methods on the API
+// See: https://docs.microsoft.com/en-us/rest/api/vsts/git/
+type GitService struct {
+	client *Client
+}
+
+// GitListRefsResponse describes the git refs list response
+type GitListRefsResponse struct {
+	Count int   `json:"count"`
+	Refs  []Ref `json:"value"`
+}
+
+// Ref describes what the git reference looks like
+type Ref struct {
+	Name     string `json:"name,omitempty"`
+	ObjectID string `json:"objectId,omitempty"`
+	URL      string `json:"url,omitempty"`
+	Statuses []struct {
+		ID          int    `json:"id,omitempty"`
+		State       string `json:"state,omitempty"`
+		Description string `json:"description,omitempty"`
+		Context     struct {
+			Name  string `json:"name,omitempty"`
+			Genre string `json:"genre,omitempty"`
+		} `json:"context,omitempty"`
+		CreationDate time.Time `json:"creationDate,omitempty"`
+		CreatedBy    struct {
+			ID          string `json:"id,omitempty"`
+			DisplayName string `json:"displayName,omitempty"`
+			UniqueName  string `json:"uniqueName,omitempty"`
+			URL         string `json:"url,omitempty"`
+			ImageURL    string `json:"imageUrl,omitempty"`
+		} `json:"createdBy,omitempty"`
+		TargetURL string `json:"targetUrl,omitempty"`
+	} `json:"statuses,omitempty"`
+}
+
+// GitRefListOptions describes what the request to the API should look like
+type GitRefListOptions struct {
+	Filter             string `url:"filter,omitempty"`
+	IncludeStatuses    bool   `url:"includeStatuses,omitempty"`
+	LatestStatusesOnly bool   `url:"latestStatusesOnly,omitempty"`
+}
+
+// ListRefs returns a list of the references for a git repo
+func (s *GitService) ListRefs(repo, refType string, opts *GitRefListOptions) ([]Ref, int, error) {
+	URL := fmt.Sprintf(
+		"/_apis/git/repositories/%s/refs/%s?api-version=4.1",
+		repo,
+		refType,
+	)
+
+	URL, err := addOptions(URL, opts)
+
+	request, err := s.client.NewRequest("GET", URL, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+	var response GitListRefsResponse
+	_, err = s.client.Execute(request, &response)
+
+	return response.Refs, response.Count, err
+}
---
+++ vsts/git_test.go
@@ -0,0 +1,99 @@
+package vsts_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/benmatselby/go-vsts/vsts"
+)
+
+const (
+	gitRefsListURL      = "/VSTS_Project/_apis/git/repositories/vscode/refs/heads"
+	gitRefsListResponse = `{
+		"count": 6,
+		"value": [
+		  {
+			"name": "refs/heads/develop",
+			"objectId": "67cae2b029dff7eb3dc062b49403aaedca5bad8d",
+			"url": "https://fabrikam.visualstudio.com/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/refs/heads/develop"
+		  },
+		  {
+			"name": "refs/heads/master",
+			"objectId": "23d0bc5b128a10056dc68afece360d8a0fabb014",
+			"url": "https://fabrikam.visualstudio.com/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/refs/heads/master"
+		  },
+		  {
+			"name": "refs/heads/npaulk/feature",
+			"objectId": "23d0bc5b128a10056dc68afece360d8a0fabb014",
+			"url": "https://fabrikam.visualstudio.com/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/refs/heads/npaulk/feature"
+		  },
+		  {
+			"name": "refs/tags/v1.0",
+			"objectId": "23d0bc5b128a10056dc68afece360d8a0fabb014",
+			"url": "https://fabrikam.visualstudio.com/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/refs/tags/v1.0"
+		  },
+		  {
+			"name": "refs/tags/v1.1",
+			"objectId": "23d0bc5b128a10056dc68afece360d8a0fabb014",
+			"url": "https://fabrikam.visualstudio.com/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/refs/tags/v1.1"
+		  },
+		  {
+			"name": "refs/tags/v2.0",
+			"objectId": "23d0bc5b128a10056dc68afece360d8a0fabb014",
+			"url": "https://fabrikam.visualstudio.com/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/refs/tags/v2.0"
+		  }
+		]
+	  }`
+)
+
+func TestGitService_ListRefs(t *testing.T) {
+	tt := []struct {
+		name     string
+		URL      string
+		response string
+		count    int
+		index    int
+		refName  string
+		refID    string
+	}{
+		{name: "return 6 refs", URL: gitRefsListURL, response: gitRefsListResponse, count: 6, index: 0, refName: "refs/heads/develop", refID: "67cae2b029dff7eb3dc062b49403aaedca5bad8d"},
+		{name: "can handle no refs returned", URL: gitRefsListURL, response: "{}", count: 0, index: -1},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			c, mux, _, teardown := setup()
+			defer teardown()
+
+			mux.HandleFunc(tc.URL, func(w http.ResponseWriter, r *http.Request) {
+				testMethod(t, r, "GET")
+				json := tc.response
+				fmt.Fprint(w, json)
+			})
+
+			opts := vsts.GitRefListOptions{}
+			refs, count, err := c.Git.ListRefs("vscode", "heads", &opts)
+			if err != nil {
+				t.Fatalf("returned error: %v", err)
+			}
+
+			if tc.index > -1 {
+				if refs[tc.index].Name != tc.refName {
+					t.Fatalf("expected git ref name %s, got %s", tc.refName, refs[tc.index].Name)
+				}
+				if refs[tc.index].ObjectID != tc.refID {
+					t.Fatalf("expected git ref object id %s, got %s", tc.refID, refs[tc.index].ObjectID)
+				}
+			}
+
+			if len(refs) != tc.count {
+				t.Fatalf("expected length of git refs to be %d; got %d", tc.count, len(refs))
+			}
+
+			if count != tc.count {
+				t.Fatalf("expected git ref count to be %d; got %d", tc.count, count)
+			}
+		})
+	}
+}
