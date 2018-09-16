1b76973c4689199fc84fcd68ddca3fa49c854b6c
Ben Selby <benmatselby@gmail.com>
2018-09-16 19:41:24 +0100

    Add some teams API calls

---
+++ azuredevops/teams.go
@@ -0,0 +1,45 @@
+package azuredevops
+
+import "fmt"
+
+// TeamsService handles communication with the teams methods on the API
+// utilising https://docs.microsoft.com/en-us/rest/api/vsts/core/teams/get%20all%20teams
+type TeamsService struct {
+	client *Client
+}
+
+// TeamsListResponse describes what the list API call returns for teams
+type TeamsListResponse struct {
+	Teams []Team `json:"value"`
+	Count int    `json:"count,omitempty"`
+}
+
+// Team describes what a team looks like
+type Team struct {
+	ID          string `url:"id,omitempty"`
+	Name        string `url:"name,omitempty"`
+	URL         string `url:"url,omitempty"`
+	Description string `url:"description,omitempty"`
+}
+
+// TeamsListOptions describes what the request to the API should look like
+type TeamsListOptions struct {
+	Mine bool `url:"$mine,omitempty"`
+	Top  int  `url:"$top,omitempty"`
+	Skip int  `url:"$skip,omitempty"`
+}
+
+// List returns list of the teams
+func (s *TeamsService) List(opts *TeamsListOptions) ([]Team, int, error) {
+	URL := fmt.Sprintf("/_apis/teams?api-version=4.1")
+	URL, err := addOptions(URL, opts)
+
+	request, err := s.client.NewBaseRequest("GET", URL, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+	var response TeamsListResponse
+	_, err = s.client.Execute(request, &response)
+
+	return response.Teams, response.Count, err
+}
---
+++ azuredevops/teams_test.go
@@ -0,0 +1,78 @@
+package azuredevops_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/benmatselby/go-azuredevops/azuredevops"
+)
+
+const (
+	teamsListURL      = "/_apis/teams"
+	teamsListResponse = `{
+		"value": [
+		  {
+			"id": "564e8204-a90b-4432-883b-d4363c6125ca",
+			"name": "Quality assurance",
+			"url": "https://fabrikam.visualstudio.com/_apis/projects/eb6e4656-77fc-42a1-9181-4c6d8e9da5d1/teams/564e8204-a90b-4432-883b-d4363c6125ca",
+			"description": "Testing staff",
+			"identityUrl": "https://fabrikam.vssps.visualstudio.com/_apis/Identities/564e8204-a90b-4432-883b-d4363c6125ca"
+		  },
+		  {
+			"id": "66df9be7-3586-467b-9c5f-425b29afedfd",
+			"name": "Fabrikam-Fiber-TFVC Team",
+			"url": "https://fabrikam.visualstudio.com/_apis/projects/eb6e4656-77fc-42a1-9181-4c6d8e9da5d1/teams/66df9be7-3586-467b-9c5f-425b29afedfd",
+			"description": "The default project team.",
+			"identityUrl": "https://fabrikam.vssps.visualstudio.com/_apis/Identities/66df9be7-3586-467b-9c5f-425b29afedfd"
+		  }
+		],
+		"count": 2
+	  }`
+)
+
+func TestTeamsService_List(t *testing.T) {
+	tt := []struct {
+		name     string
+		URL      string
+		title    string
+		response string
+		count    int
+	}{
+		{name: "can identify teams in the response", URL: teamsListURL, response: teamsListResponse, count: 2, title: "Quality assurance"},
+		{name: "there are no items in the response", URL: teamsListURL, response: "{}", count: 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			c, mux, _, teardown := setup()
+			defer teardown()
+
+			mux.HandleFunc(tc.URL, func(w http.ResponseWriter, r *http.Request) {
+				testMethod(t, r, "GET")
+				json := tc.response
+				fmt.Fprint(w, json)
+			})
+
+			opt := &azuredevops.TeamsListOptions{}
+			response, count, err := c.Teams.List(opt)
+			if err != nil {
+				t.Fatalf("returned error: %v", err)
+			}
+
+			if count != tc.count {
+				t.Fatalf("expected count in response to be %d; got %d", tc.count, count)
+			}
+
+			if len(response) != tc.count {
+				t.Fatalf("expected length of response to be 0; got %d", len(response))
+			}
+
+			if count > 0 {
+				if tc.title != response[0].Name {
+					t.Fatalf("expected name to be %s; got %s", tc.title, response[0].Name)
+				}
+			}
+		})
+	}
+}
